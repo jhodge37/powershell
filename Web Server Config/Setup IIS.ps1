183d9b96e7720d7c5aa25e1e3b0bb0972b3e7023
James Hodge <james.hodge@ert.com>
2017-05-31 20:51:41 +0100

    Updated 31/5/17

--- Web Server Config/Setup IIS.ps1
+++ Web Server Config/Setup IIS.ps1
@@ -1,38 +1,34 @@
-
+$env = 'int'
+$apps = ('Testapp1','Testapp2','Testapp3')
 $servers = ('c1sgtrvap01','c1dgtrvap01','c1dgtrvap02')
-foreach ($server in $servers) 
-{
-$apps = ('Testapp1', 'Testapp2', 'Testapp3')
-foreach ($app in $apps)
-$scriptBlock = {
+foreach ($server in $servers) {
+    foreach ($app in $apps) {
+        $scriptBlock = {
+            #ImportModules
+            Import-Module WebAdministration
+            
 
-#ImportModules
-Import-Module WebAdministration
+            $cred = Get-credential
+                        
+            if(Test-Path IIS:\AppPools\$app) {
+                Write-Output "App pool exists - removing"
+                Remove-WebAppPool $app
+                Get-ChildItem IIS:\AppPools
+            }
+            New-WebAppPool -Name $app
+            $appPool = Get-Item "IIS:\AppPools\$app"
+            $appPool.processModel.identityType = 3
+            $appPool.processModel.username = ($cred.domain+'\'+$cred.username)
+            $appPool.processModel.password = $cred.Password
+            $appPool.managedRuntimeVersion = "v4.0"
+            $appPool.managedPipeLineMode = "Classic"
+            $appPool | Set-Item
+            
+            #create websites
+            New-Item iis:\Sites\$app -bindings @{protocol="http";bindingInformation="(':80:'+$app+'.'+$env+'.ert.com')"} -physicalPath C:\inetpub\wwwroot\
 
-#create app pools 
-$ID = "ERT\gth_atlas_test"
-$cred = Get-Credential -Credential $ID
-$appPoolName = "TestAppPool2"
-if(Test-Path IIS:\AppPools\$appPoolName)  
-{
-    Write-Output "App pool exists - removing"
-    Remove-WebAppPool $appPoolName
-    Get-ChildItem IIS:\AppPools
-}
-New-WebAppPool -Name $appPoolName
-$appPool = Get-Item "IIS:\AppPools\$appPoolName"
-$appPool.processModel.identityType = 3
-$appPool.processModel.username = $ID
-$appPool.processModel.password = $cred.GetNetworkCredential().Password
-$appPool.managedRuntimeVersion = "v4.0"
-$appPool.managedPipeLineMode = "Classic"
-$appPool | Set-Item
+            }
 
-
-#create websites
-New-Item iis:\Sites\Atlas -bindings @{protocol="http";bindingInformation=":80:atlas.int.ert.com"} -physicalPath C:\inetpub\wwwroot\
-
-
-
-}
-Invoke-Command –ComputerName $server –ScriptBlock $scriptBlock
+        }
+        Invoke-Command –ComputerName $server –ScriptBlock $scriptBlock
+    }
