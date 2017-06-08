8ef074a7dd1fb2fca2754277eb6139dffdf1f991
James Hodge <james.hodge@ert.com>
2017-06-08 10:34:11 +0100

    Excluded Default Website

--- Web Server Config/Repair.ps1
+++ Web Server Config/Repair.ps1
@@ -4,6 +4,6 @@ Get-ChildItem -Path IIS:\AppPools | foreach {
     Start-WebAppPool $_.Name;
 }
 
-Get-ChildItem -Path IIS:\Sites | foreach {
+Get-ChildItem -Path IIS:\Sites | Where-Object {$_.Name -notmatch "Default Web Site"} | foreach {
     Start-WebSite $_.Name; 
 }
