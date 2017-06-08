428a6c93dc696f655cf1f274258d33da23453b57
James Hodge <james.hodge@ert.com>
2017-06-08 11:19:02 +0100

    Added remote execution

--- Web Server Config/Repair.ps1
+++ Web Server Config/Repair.ps1
@@ -1,15 +1,24 @@
 Import-Module WebAdministration
 
-$AppPools = gci -Path IIS:\AppPools
-$Sites = gci -Path IIS:\Sites | Where-Object {$_.Name -notmatch "Default Web Site"}
-foreach ($AppPool IN $AppPools) {
-    Start-WebAppPool $AppPool.name
+$Server = $args[0]
+
+$Scriptblock = {
+    $AppPools = gci -Path IIS:\AppPools
+    $Sites = gci -Path IIS:\Sites | Where-Object {$_.Name -notmatch "Default Web Site"}
+    foreach ($AppPool IN $AppPools) {
+        Start-WebAppPool $AppPool.name
+    }
+
+
+    foreach ($Site IN $Sites) {
+        Start-WebSite $Site.name 
+    }
+
+    $StoppedSites = GCI -Path IIS:\Sites | Where-Object {
+        $_.Name -notmatch "Default Web Site" -and $_.State -match "Stopped"
+        }
+    if ($StoppedSites) {
+        iisreset /rebootonerror
+    }
 }
-
-
-foreach ($Site IN $Sites) {
-    Start-WebSite $Site.name 
-}
-
-$StoppedSites = GCI -Path IIS:\Sites | Where-Object {$_.Name -notmatch "Default Web Site" -and $_.State -match "Stopped"}
-if ($StoppedSites) {iisreset /rebootonerror}
+Invoke-Command –ComputerName $server –ScriptBlock $scriptBlock
